345d9ad4ea628b44bb7aa6015a8595eccde4991f
54679128 <a54679128@outlook.com>
2025-11-28 18:31:00 +0800

    修复测试代码中的调用方式

--- src/1/test/fluidTest.lua
+++ src/1/test/fluidTest.lua
@@ -5,7 +5,7 @@ local command = require("TransferCommand.TCFluid")
 -- 测试环境：将src文件夹复制到目标计算机，在运行该测试的计算机或海龟两侧放置流体容器，初始时向左侧容器注入适当流体
 -- 注意事项：保证容器没有某种访问限制
 
-local testInvoke = invoke:new()
+local testInvoke = invoke()
 testInvoke:addCommand(command("left", "right"))
 testInvoke:addCommand(command("right", "left"))
 
--- src/1/test/invTest.lua
+++ src/1/test/invTest.lua
@@ -4,7 +4,7 @@ local invoke = require("CommandInvoker")
 local command = require("TransferCommand.TCItemInventory")
 -- 测试环境：将src文件夹复制到目标计算机，在运行该测试的计算机或海龟两侧放置物品容器，初始时左侧容器放置若干物品
 
-local testInvoke = invoke:new()
+local testInvoke = invoke()
 
 testInvoke:addCommand(command("left", "right"))
 
