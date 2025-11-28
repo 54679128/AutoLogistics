dec98d800bce3762ebbdfa4e6dbeaf0349cd3ba7
54679128 <a54679128@outlook.com>
2025-11-28 18:11:10 +0800

    添加流体转移测试

---
+++ src/1/test/fluidTest.lua
@@ -0,0 +1,11 @@
+require("_steup")
+
+local invoke = require("CommandInvoker")
+local command = require("TransferCommand.TCFluid")
+-- 测试环境：将src文件夹复制到目标计算机，在运行该测试的计算机或海龟两侧放置流体容器，初始时向左侧容器注入适当流体
+
+local testInvoke = invoke:new()
+testInvoke:addCommand(command("left", "right"))
+testInvoke:addCommand(command("right", "left"))
+
+print(textutils.serialise(testInvoke:processAll(), { allow_repetitions = true }))
