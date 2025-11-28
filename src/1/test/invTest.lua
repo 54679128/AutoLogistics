3da6cb7a4a3188ac3f2567a68095fd6d723d2ead
54679128 <a54679128@outlook.com>
2025-11-28 09:38:36 +0800

    修改测试内容，去除无用代码

--- src/1/test/invTest.lua
+++ src/1/test/invTest.lua
@@ -1,14 +1,15 @@
 require("_stesuo")
-print(package.path)
-sleep(5)
+
 local invoke = require("CommandInvoker")
-print("success get invoke")
 local command = require("TransferCommand.TCItemInventory")
-print("success get command")
 -- 测试环境：将src文件夹复制到目标计算机，在运行该测试的计算机或海龟两侧放置大小相等的容器，初始时左侧容器放置若干物品
 
 local testInvoke = invoke:new()
 
-testInvoke:addCommand(command("left", "right"))
-testInvoke:addCommand(command("right", "left"))
-testInvoke:processAll()
+testInvoke:addCommand(command("left", "top"))
+testInvoke:addCommand(command("top", "right"))
+testInvoke:addCommand(command("right", "bottom"))
+testInvoke:addCommand(command("bottom", "left"))
+while true do
+    testInvoke:processAll()
+end
