b646896c72ceca223defe0801b47326bf4cddf03
54679128 <a54679128@outlook.com>
2025-11-28 17:38:50 +0800

    修改测试描述

--- src/1/test/invTest.lua
+++ src/1/test/invTest.lua
@@ -2,7 +2,7 @@ require("_steup")
 
 local invoke = require("CommandInvoker")
 local command = require("TransferCommand.TCItemInventory")
--- 测试环境：将src文件夹复制到目标计算机，在运行该测试的计算机或海龟两侧放置大小相等的容器，初始时左侧容器放置若干物品
+-- 测试环境：将src文件夹复制到目标计算机，在运行该测试的计算机或海龟两侧放置物品容器，初始时左侧容器放置若干物品
 
 local testInvoke = invoke:new()
 
@@ -12,4 +12,5 @@ testInvoke:addCommand(command("right", "left"))
 
 local result = testInvoke:processAll()
 
+-- 该程序会打印两次转移物品的数量。如果两侧容器均可完全容纳最初放置的物品，这两个值应该相等
 print(textutils.serialise(result, { allow_repetitions = true }))
