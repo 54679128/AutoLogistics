021ca2885fa2b4f7f5ee642089d02fecd880e81e
54679128 <a54679128@outlook.com>
2025-12-03 10:52:20 +0800

    添加了物品（理论上流体也行）过滤器和一组预定义的过滤器

---
+++ src/2/Filter.lua
@@ -0,0 +1,49 @@
+local O = require("lib.Object")
+
+---@class a546.ItemStack
+---@field count number
+---@field displayName string|nil
+---@field itemGroups {displayName:string,id:string}[]|{}
+---@field maxCount number|nil
+---@field name string
+---@field tags table<string,boolean>|nil
+---@field nbt string|nil
+
+---@class a546.Filter
+---@field predicate fun(ItemStack:a546.ItemStack):boolean
+local Filter = O:extend()
+
+---@cast Filter +fun(predicate:fun(ItemStack:a546.ItemStack):boolean):a546.Filter
+function Filter:new(predicate)
+    self.predicate = predicate or function()
+        return true
+    end
+end
+
+--- AND
+---@param filiter a546.Filter
+---@return a546.Filter
+function Filter:And(filiter)
+    return Filter(function(ItemStack)
+        return self.predicate(ItemStack) and filiter.predicate(ItemStack)
+    end)
+end
+
+--- OR
+---@param filiter a546.Filter
+---@return a546.Filter
+function Filter:Or(filiter)
+    return Filter(function(ItemStack)
+        return self.predicate(ItemStack) or filiter.predicate(ItemStack)
+    end)
+end
+
+--- NOT
+---@return a546.Filter
+function Filter:Not()
+    return Filter(function(ItemStack)
+        return not self.predicate(ItemStack)
+    end)
+end
+
+return Filter
