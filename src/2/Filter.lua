fd5caff91907d266052bd76d5950bd21e16d8374
54679128 <a54679128@outlook.com>
2025-12-04 17:29:44 +0800

    为 ItemStack 添加关于流体情况的描述

--- src/2/Filter.lua
+++ src/2/Filter.lua
@@ -1,7 +1,7 @@
 local O = require("lib.Object")
 
 ---@class a546.ItemStack
----@field count number
+---@field count number 对于流体，这代表 amount
 ---@field displayName string|nil
 ---@field itemGroups {displayName:string,id:string}[]|{}
 ---@field maxCount number|nil
