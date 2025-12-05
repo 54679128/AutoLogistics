a3c4e85717a315a943061957dcf2147e30408aff
54679128 <a54679128@outlook.com>
2025-12-05 16:54:56 +0800

    在公用配置模块中添加日志输出路径配置

--- resource/_steup.lua
+++ resource/_steup.lua
@@ -8,3 +8,6 @@ local scriptSrcPath = debug.getinfo(1, "S").source:sub(2):match("(.*src[/\\])")
 insertPath(scriptSrcPath .. "?.lua")
 insertPath(scriptSrcPath .. "1/?.lua")
 insertPath(scriptSrcPath .. "2/?.lua")
+
+local log = require("lib.log")
+log.outfile = "log.txt"
