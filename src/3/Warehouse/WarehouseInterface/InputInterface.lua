bf02be8b9da3018381c0788701856aa5913a24a1
54679128 <a54679128@outlook.com>
2025-12-27 19:59:15 +0800

    feat(WarehouseM): 添加WarehouseInputInterface和WarehouseOutputInterface类以支持仓库输入输出功能

---
+++ src/3/Warehouse/WarehouseInterface/InputInterface.lua
@@ -0,0 +1,20 @@
+local InterfaceBase   = require "Warehouse.WarehouseInterface.InterfaceBase"
+local ContainerStackM = require "ContainerStack.ContainerStackM"
+local Filter          = require "Filter"
+
+---@class a546.WarehouseInputInterface:a546.WarehouseInterfaceBase
+local input           = InterfaceBase:extend()
+
+---@cast input +fun(warehouseM:a546.WarehouseM):a546.WarehouseInputInterface
+function input:new(warehouseM, peripheralName)
+    ---@diagnostic disable-next-line: redundant-parameter
+    self.super.new(self, warehouseM, peripheralName)
+end
+
+function input:mayer()
+    self.warehouse:input(ContainerStackM(self.peripheralName), Filter(function(resource)
+        return true
+    end))
+end
+
+return input
---
+++ src/3/Warehouse/WarehouseInterface/outputInterface.lua
@@ -0,0 +1,20 @@
+local InterfaceBase   = require "Warehouse.WarehouseInterface.InterfaceBase"
+local ContainerStackM = require "ContainerStack.ContainerStackM"
+local Filter          = require "Filter"
+
+---@class a546.WarehouseOutputInterface:a546.WarehouseInterfaceBase
+local output          = InterfaceBase:extend()
+
+---@cast output +fun(warehouseM:a546.WarehouseM):a546.WarehouseOutputInterface
+function output:new(warehouseM, peripheralName)
+    ---@diagnostic disable-next-line: redundant-parameter
+    self.super.new(self, warehouseM, peripheralName)
+end
+
+function output:mayer()
+    self.warehouse:output(ContainerStackM(self.peripheralName), Filter(function(resource)
+        return true
+    end))
+end
+
+return output
