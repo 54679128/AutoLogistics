670769e3485f592c8029f3defc856e8358336131
54679128 <a54679128@outlook.com>
2026-01-10 14:16:30 +0800

    feat:允许`Filter`选择/过滤一项资源的一部分作为`SearchResult`

--- src/2/Filter.lua
+++ src/2/Filter.lua
@@ -3,11 +3,11 @@ local util = require("lib.util")
 
 -- 注意，谓词不应该修改参数
 ---@class a546.Filter
----@field predicate fun(resource:a546.Resource):boolean
+---@field predicate fun(resource:a546.Resource):boolean,number?
 ---@field name string
 local Filter = O:extend()
 
----@cast Filter +fun(predicate:(fun(resource:a546.Resource):boolean),name?:string):a546.Filter
+---@cast Filter +fun(predicate:(fun(resource:a546.Resource):boolean,number?),name?:string):a546.Filter
 function Filter:new(predicate, name)
     self.predicate = predicate or function()
         return true
@@ -24,7 +24,14 @@ end
 ---@return a546.Filter
 function Filter:And(filter)
     return Filter(function(resource)
-        return self.predicate(resource) and filter.predicate(resource)
+        local take1, howMuch1 = self.predicate(resource)
+        local take2, howMuch2 = filter.predicate(resource)
+        ---@type number|nil
+        local howMuch
+        if howMuch1 and howMuch2 then
+            howMuch = math.min(howMuch1, howMuch2)
+        end
+        return take1 and take2, howMuch
     end)
 end
 
@@ -33,7 +40,14 @@ end
 ---@return a546.Filter
 function Filter:Or(filter)
     return Filter(function(resource)
-        return self.predicate(resource) or filter.predicate(resource)
+        local take1, howMuch1 = self.predicate(resource)
+        local take2, howMuch2 = filter.predicate(resource)
+        ---@type number|nil
+        local howMuch
+        if howMuch1 and howMuch2 then
+            howMuch = math.max(howMuch1, howMuch2)
+        end
+        return take1 or take2, howMuch
     end)
 end
 
