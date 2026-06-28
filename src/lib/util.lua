9ae0b99a1b8179d4a677b8ec15b2da34bb768165
54679128 <a54679128@outlook.com>
2026-06-28 17:13:28 +0800

    feat(util): 添加`readOnly`工具，返回一个表的只读副本

--- src/lib/util.lua
+++ src/lib/util.lua
@@ -59,4 +59,71 @@ function out.len(tTable)
     return i
 end
 
+local cache = setmetatable({}, { __mode = "kv" })
+--- 返回一个表的只读代理
+---@generic T:table
+---@param theTable T
+---@return T
+function out.readOnly(theTable)
+    if cache[theTable] then
+        return cache[theTable]
+    end
+    local proxy = {}
+    local pMetaTable = {}
+
+    cache[theTable] = proxy -- 缓存真实表
+    cache[proxy] = proxy    -- 缓存只读代理
+
+    pMetaTable.__index = function(t, k)
+        local v = theTable[k]
+        if type(v) == "table" then
+            return out.readOnly(v)
+        elseif type(v) == "function" then
+            return function(firstParam, ...)
+                -- local meta = getmetatable(firstParam)
+                -- while meta do
+                --     if meta == proxy then
+                --         return v(theTable, ...)
+                --     end
+                --     meta = getmetatable(meta)
+                -- end
+                if firstParam == proxy then
+                    return v(theTable, ...)
+                else
+                    return v(firstParam, ...)
+                end
+            end
+        else
+            return v
+        end
+    end
+    pMetaTable.__newindex = function(t, k, v)
+        error(("Can't set table: %s, key: %s to value: %s"):format(tostring(theTable), tostring(k), tostring(v)), 2)
+    end
+    pMetaTable.__len = function(t)
+        return #theTable
+    end
+    pMetaTable.__pairs = function()
+        return function(_, oldValue)
+            local key, newValue = next(theTable, oldValue)
+            if type(newValue) == "table" then
+                return key, out.readOnly(newValue)
+            else
+                return key, newValue
+            end
+        end, nil, nil
+    end
+    pMetaTable.__call = function(_, ...)
+        return theTable(...)
+    end
+    local originMetaTable = getmetatable(theTable)
+    if originMetaTable then
+        pMetaTable.__metatable = out.readOnly(getmetatable(theTable))
+    else
+        pMetaTable.__metatable = {}
+    end
+    setmetatable(proxy, pMetaTable)
+    return proxy
+end
+
 return out
